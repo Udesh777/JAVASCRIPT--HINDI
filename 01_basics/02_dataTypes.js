315d7ec15f75275c2a8b03a0be894a7653723a72
Udesh777 <144572447+Udesh777@users.noreply.github.com>
2024-11-13 21:45:52 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" //treat all js code as newer version
+//alert(3+3) we are using node js not browser
+
+//Code should be Readable
+console.log("UDESH") ;console.log(3+3)
+
+let name = "UDESH"   //string
+let age = 18;        //number
+let isLogedIn = false   //Boolean
+
+console.table([name ,age,isLogedIn])
+console.table([typeof name , typeof age, typeof isLogedIn])
+//NULL AND UNDEFINED
+//NULL :- The null value represents the intentional absence of any object value . To be explicitly assigned.
+let a = null
+console.log(a)
+console.log(typeof a)
+
+//undefined :- A variable that has not been assigned a value is of type undefined
+let b;
+console.log(b)
+console.log(typeof b)
